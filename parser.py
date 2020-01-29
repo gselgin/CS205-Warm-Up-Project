d6db2330a72f1d10d6e9ab9834eaad37c3eac350
gregelgin <gselgin@uvm.edu>
2020-01-29 09:22:50 -0500

    Update 1

--- parser.py
+++ parser.py
@@ -4,6 +4,9 @@
 
 
 # TODO: TOKENS: Region, Month, type (conventional vs organic), more?
+# region (type, month)
+# or 
+# sales (all of fields)
 def main():
     get_info()
 
@@ -15,6 +18,7 @@ def main():
 
 
 def get_info():
+    print()
     print("Welcome to the Avocado data parsing program")
     print("You can search for avocado price and sales by region and date")
     # TODO: Add more info
@@ -26,22 +30,36 @@ def get_user_input():
 
 
 def parse(user_input):
-    # Initialize local variables
-    input_list = list(user_input.split(" "))
-    user_query_list = list()
-    user_query = ""
+    # Initialize valid to enter while loop
+    valid = False
 
-    # If the first word in the search is region
-    # Then the first part of the query is region followed by the region name
-    if input_list[0] == "region":
-        user_query_list.append("SELECT")
-        user_query_list.append(input_list[1])
-        user_query_list.append("FROM")
-        user_query_list.append("region")
+    while not valid:
+        # Initialize local variables
+        input_list = list(user_input.split(" "))
+        user_query_list = list()
+        user_query = ""
 
-        if input_list[2] == "type":
-            user_query_list.append("WHERE")
-            user_query_list.append(input_list[3] + "=type")
+        # If the first word in the search is "region"
+        # Then the first part of the query is "region" followed by the region name
+        if input_list[0] == "region":
+            valid = True
+            user_query_list.append("SELECT")
+            user_query_list.append(input_list[1])
+            user_query_list.append("FROM")
+            user_query_list.append("region")
+
+            if len(input_list) >= 4:
+                if input_list[2] == "type":
+                    user_query_list.append("WHERE")
+                    user_query_list.append("type=" + input_list[3])
+            else:
+                print("empty")
+
+        if len(user_query_list) == 0:
+            valid = False
+            get_info()
+            get_user_input()
+
 
     # Turn query list into query string
     for word in user_query_list:
