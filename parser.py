ba4ab2370632346de2a714119ec9c586afc80282
gregelgin <gselgin@uvm.edu>
2020-02-10 12:43:38 -0500

    Update 3

--- parser.py
+++ parser.py
@@ -1,29 +1,27 @@
 # Greg Elgin, Connor Hamilton
 # CS 205: Warm up project
+# Last Updated: 02/10/20
 # Parsing system to take a string input and return token values
 # Calls query function with tokens as parameters
 
-
 # TODO: TOKENS: Region, Month, type (conventional vs organic), more? Figure out with SQL team
-# token list index values region
-# [region, AveragePrice, Total Volume, Date, type ]
-# token list index values sales
-# [type, month, price, num sales, region]
+
+import shlex
+
+region_list = ["Great Lakes", "Harrisburg Scranton", "Hartford Springfield", "Houston", "Indianapolis", "Jacksonville",
+               "Las Vegas", "Los Angeles", "Louisville", "Miami Ft Lauderdale", "Nashville", "New Orleans Mobile",
+               "New York", "Northeast", "Northern New England"]
 
 
 def main():
-    print()
     print("Welcome to the Avocado data parsing program")
     get_info()
-
     parse()
 
 
 def get_info():
     print()
-    print("You can search avocado date by region or sales")
-    print("Begin your search by typing 'region' followed by a region name")
-    print("or 'sales' followed by the avocado type (conventional or organic)")
+    print("You can search for avocado average price or total volume")
     print("Enter 'region list' for list of regions")
     print("Enter q to quit")
     # TODO: Add more info
@@ -33,109 +31,80 @@ def get_user_input():
     usr_input = str(input(""))
     return usr_input
 
-# function to return a list of regions
+
 def get_region_list():
-    region_list = [
-        "GreatLakes",
-        "HarrisburgScranton",
-        "HartfordSpringfield",
-        "Houston",
-        "Indianapolis",
-        "Jacksonville",
-        "LasVegas",
-        "LosAngeles",
-        "Louisville",
-        "MiamiFtLauderdale",
-        "Nashville",
-        "NewOrleansMobile",
-        "NewYork",
-        "Northeast",
-        "NorthernNewEngland"
-    ]
     return region_list
 
 
 def parse():
     # Initialize valid to enter while loop
     valid = False
-    finish = False
+    # Initialize search to allow for user to quit without searching
+    search = False
 
-    while not valid or not finish:
-
-        # get user input
-
+    while not valid:
+        # get user input, turn into list of words, create empty token list
         user_input = get_user_input()
-        input_list = list(user_input.split(" "))
+        user_input = user_input.lower()
+        input_list = shlex.split(user_input)
         tokens = list()
 
-        # If the first word in the search is "region"
-        # Then the first token is the word after region
-        if input_list[0] == "region":
-            valid = True
-            search = True
+        # Save length of user input
+        length = len(input_list)
 
-            tokens.append(input_list[1])
+        # If user types "region list" display a list of regions
+        if length >= 2 and input_list[0] == "region" and input_list[1] == "list":
+            print("Region list: ")
+            for item in region_list:
+                print(item)
+            print()
 
-            if len(input_list) == 2 and input_list[1] == "list":
-                print("Region list: ")
-                print(get_region_list())
-                search = False
+        # First word needs to be "average price" or "total volume"
+        if length >= 2 and input_list[0] == "average" and input_list[1] == "price" or \
+                input_list[0] == "total" and input_list[1] == "volume":
+            if input_list[0] == "average" and input_list[1] == "price":
+                tokens.append("AveragePrice")
+            elif input_list[0] == "total" and input_list[1] == "volume":
+                tokens.append("TotalVolume")
 
-            # placeholder tokens
-            token_list = ["averagePrice", "Total Volume", "Date", "type"]
+            # Next word should be 'region' followed by a valid region name
+            if length >= 4 and input_list[2] == "region":
+                region_list_lower = [x.lower() for x in region_list]
+                if input_list[3] in region_list_lower:
+                    tokens.append(input_list[3])
 
+                    # FOR TESTING PURPOSES
+                    valid = True
+                    query(tokens)
 
-
-            # search for second token if exists
-            if len(input_list) >= 3:
-                if input_list[2] in token_list:
-                    tokens.append(input_list[2])
-                    query_region(tokens)
                 else:
+                    print("That is not a valid region")
+                    print("Input 'region list' to see valid regions")
+                    print("If region is multiple words input it in quotes")
                     valid = False
-                    print("Invalid token in region")
-            elif search:
-                query_region(tokens)
+            else:
+                print("Please input 'region' followed by a region after you specify average price or total volume")
+                valid = False
 
-        # If the first word in the search is "sales"
-        # Then the first token is the word after sales (conventional or organic)
-        elif input_list[0] == "sales":
-            valid = True
-            tokens.append(input_list[1])
-            query_sales(tokens)
+            # Next item should be 'month' followed by an int 1-12
 
-            # placeholder tokens
-            token_list = ["type", "month", "price", "num sales", "region"]
+            # Last item should be 'type' followed by 'conventional' or 'organic'
 
-            # search for second token if exists
-            if len(input_list) >= 3:
-                if input_list[2] in token_list:
-                    tokens.append(input_list[2])
-                else:
-                    valid = False
-                    print("Invalid search")
+
 
         # exit program if input is "q"
-        elif input_list[0].lower() == "q":
+        elif length >= 1 and input_list[0] == "q":
             valid = True
-            finish = True
+            search = False
 
         else:
+            print("Please begin your search with 'average price' or 'total volume")
+            print("Or input 'q' to quit")
             valid = False
-            print("Invalid search: ")
-            get_info()
-            get_user_input()
-
-
-
 
 
 # FOR TESTING PURPOSES
-def query_sales(tokens):
-    print ("query_sales")
-    print(tokens)
-def query_region(tokens):
-    print ("query_region")
+def query(tokens):
     print(tokens)
 
 
